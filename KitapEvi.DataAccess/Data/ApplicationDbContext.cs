21865740310a6e6db55bfa5381b9e7e9c8448b25
Zidan Omar Hamid <hzidanomar@gmail.com>
2021-12-21 16:04:30 +0300

    Add project files.

---
+++ KitapEvi.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KitapEvi.DataAccess.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Category> Category { get; set; }
+	}
+}
